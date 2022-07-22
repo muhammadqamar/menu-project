43900017734a3800f62db0e3778179a8b0af0a44
muhammadqamar <muhammadqamar111@gmail.com>
2022-07-22 19:54:16 +0500

    card design

--- src/component/homePage/index.js
+++ src/component/homePage/index.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { Box, Text, Link } from '@chakra-ui/react';
-import Main from './main';
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+// section
+import Menus from './menus';
+
+// images
+import heroBgImage from '../assets/images/bg.png';
 
 function Index() {
   return (
-    <Box>
-      <Text>Menu Project</Text>
-      <Link
-        color="teal.500"
-        href="https://chakra-ui.com"
-        fontSize="2xl"
-        target="_blank"
-        rel="noopener noreferrer"
+    <>
+      <Box
+        w="100%"
+        h="710px"
+        __css={{
+          backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.8) 9.89%, rgba(0, 0, 0, 0.8) 100%), url(${heroBgImage})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
       >
-        Learn Chakra
-      </Link>
-      <Main />
-    </Box>
+        <Flex
+          maxW="1440px"
+          h="100%"
+          m="0 auto"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text
+            fontFamily="Crimson Pro"
+            fontSize="58px"
+            fontWeight="normal"
+            lineHeight="63.8px"
+            letterSpacing="8.7px"
+            textTransform="uppercase"
+            color="white"
+          >
+            Menu
+          </Text>
+        </Flex>
+      </Box>
+
+      {/* section*/}
+
+      <Menus />
+    </>
   );
 }
 
